ae7ef7640f293eb811903966bdca6ef553ed5ff3
BrianReckliss <Brian.Rad91@gmail.com>
2013-12-08 10:56:33 -0700

    C# Reduex

--- Abandon_All_Hope/Abandon_All_Hope/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// Abandon_All_Hope.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-

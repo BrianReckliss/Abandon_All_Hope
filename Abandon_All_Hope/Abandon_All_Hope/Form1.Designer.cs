a55dcd15d09d39ff99badc30eb2ac89fba934fb5
BrianReckliss <Brian.Rad91@gmail.com>
2013-12-08 15:38:20 -0700

    Wumbo

--- Abandon_All_Hope/Abandon_All_Hope/Form1.Designer.cs
+++ Abandon_All_Hope/Abandon_All_Hope/Form1.Designer.cs
@@ -31,26 +31,35 @@
             this.btnOpen = new System.Windows.Forms.Button();
             this.bGroupTagging = new System.Windows.Forms.GroupBox();
             this.lblTags = new System.Windows.Forms.Label();
+            this.lblDir = new System.Windows.Forms.Label();
             this.rbtnNSFW = new System.Windows.Forms.RadioButton();
             this.rbtnSFW = new System.Windows.Forms.RadioButton();
             this.lblSafe = new System.Windows.Forms.Label();
-            this.lblDir = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picBox1 = new System.Windows.Forms.PictureBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.TabContainer = new System.Windows.Forms.TabControl();
             this.ViewTab = new System.Windows.Forms.TabPage();
+            this.btnNextImg = new System.Windows.Forms.Button();
+            this.btnScale = new System.Windows.Forms.Button();
+            this.btnPrevImg = new System.Windows.Forms.Button();
             this.SearchTab = new System.Windows.Forms.TabPage();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.bGroupTagging.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
             this.TabContainer.SuspendLayout();
             this.ViewTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(10, 41);
+            this.btnOpen.Location = new System.Drawing.Point(6, 19);
             this.btnOpen.Name = "btnOpen";
-            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.Size = new System.Drawing.Size(75, 25);
             this.btnOpen.TabIndex = 0;
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = true;
@@ -59,13 +68,15 @@
             // bGroupTagging
             // 
             this.bGroupTagging.Controls.Add(this.lblTags);
+            this.bGroupTagging.Controls.Add(this.lblDir);
             this.bGroupTagging.Controls.Add(this.rbtnNSFW);
             this.bGroupTagging.Controls.Add(this.btnOpen);
             this.bGroupTagging.Controls.Add(this.rbtnSFW);
             this.bGroupTagging.Controls.Add(this.lblSafe);
-            this.bGroupTagging.Location = new System.Drawing.Point(574, 5);
+            this.bGroupTagging.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bGroupTagging.Location = new System.Drawing.Point(0, 0);
             this.bGroupTagging.Name = "bGroupTagging";
-            this.bGroupTagging.Size = new System.Drawing.Size(205, 553);
+            this.bGroupTagging.Size = new System.Drawing.Size(261, 562);
             this.bGroupTagging.TabIndex = 2;
             this.bGroupTagging.TabStop = false;
             this.bGroupTagging.Text = "Tagging:";
@@ -73,16 +84,26 @@
             // lblTags
             // 
             this.lblTags.AutoSize = true;
-            this.lblTags.Location = new System.Drawing.Point(5, 153);
+            this.lblTags.Location = new System.Drawing.Point(11, 100);
             this.lblTags.Name = "lblTags";
             this.lblTags.Size = new System.Drawing.Size(34, 13);
             this.lblTags.TabIndex = 3;
             this.lblTags.Text = "Tags:";
             // 
+            // lblDir
+            // 
+            this.lblDir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblDir.AutoSize = true;
+            this.lblDir.Location = new System.Drawing.Point(3, 546);
+            this.lblDir.Name = "lblDir";
+            this.lblDir.Size = new System.Drawing.Size(64, 13);
+            this.lblDir.TabIndex = 3;
+            this.lblDir.Text = "(Directory...)";
+            // 
             // rbtnNSFW
             // 
             this.rbtnNSFW.AutoSize = true;
-            this.rbtnNSFW.Location = new System.Drawing.Point(114, 18);
+            this.rbtnNSFW.Location = new System.Drawing.Point(104, 68);
             this.rbtnNSFW.Name = "rbtnNSFW";
             this.rbtnNSFW.Size = new System.Drawing.Size(57, 17);
             this.rbtnNSFW.TabIndex = 2;
@@ -93,7 +114,7 @@
             // rbtnSFW
             // 
             this.rbtnSFW.AutoSize = true;
-            this.rbtnSFW.Location = new System.Drawing.Point(59, 18);
+            this.rbtnSFW.Location = new System.Drawing.Point(49, 68);
             this.rbtnSFW.Name = "rbtnSFW";
             this.rbtnSFW.Size = new System.Drawing.Size(49, 17);
             this.rbtnSFW.TabIndex = 1;
@@ -104,29 +125,23 @@
             // lblSafe
             // 
             this.lblSafe.AutoSize = true;
-            this.lblSafe.Location = new System.Drawing.Point(7, 20);
+            this.lblSafe.Location = new System.Drawing.Point(11, 70);
             this.lblSafe.Name = "lblSafe";
             this.lblSafe.Size = new System.Drawing.Size(32, 13);
             this.lblSafe.TabIndex = 0;
             this.lblSafe.Text = "Safe:";
             // 
-            // lblDir
+            // picBox1
             // 
-            this.lblDir.AutoSize = true;
-            this.lblDir.Location = new System.Drawing.Point(5, 544);
-            this.lblDir.Name = "lblDir";
-            this.lblDir.Size = new System.Drawing.Size(64, 13);
-            this.lblDir.TabIndex = 3;
-            this.lblDir.Text = "(Directory...)";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 8);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
+            this.picBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox1.Location = new System.Drawing.Point(0, 0);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(511, 499);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox1.TabIndex = 4;
+            this.picBox1.TabStop = false;
             // 
             // openFileDialog1
             // 
@@ -136,51 +151,106 @@
             // 
             this.TabContainer.Controls.Add(this.ViewTab);
             this.TabContainer.Controls.Add(this.SearchTab);
-            this.TabContainer.Location = new System.Drawing.Point(8, 12);
+            this.TabContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabContainer.Location = new System.Drawing.Point(0, 0);
             this.TabContainer.Name = "TabContainer";
             this.TabContainer.SelectedIndex = 0;
-            this.TabContainer.Size = new System.Drawing.Size(551, 546);
+            this.TabContainer.Size = new System.Drawing.Size(519, 562);
             this.TabContainer.TabIndex = 5;
             // 
             // ViewTab
             // 
-            this.ViewTab.Controls.Add(this.pictureBox1);
+            this.ViewTab.Controls.Add(this.btnNextImg);
+            this.ViewTab.Controls.Add(this.btnScale);
+            this.ViewTab.Controls.Add(this.btnPrevImg);
+            this.ViewTab.Controls.Add(this.picBox1);
             this.ViewTab.Location = new System.Drawing.Point(4, 22);
             this.ViewTab.Name = "ViewTab";
             this.ViewTab.Padding = new System.Windows.Forms.Padding(3);
-            this.ViewTab.Size = new System.Drawing.Size(543, 520);
+            this.ViewTab.Size = new System.Drawing.Size(511, 536);
             this.ViewTab.TabIndex = 0;
             this.ViewTab.Text = "View";
             this.ViewTab.UseVisualStyleBackColor = true;
+            // 
+            // btnNextImg
+            // 
+            this.btnNextImg.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnNextImg.Location = new System.Drawing.Point(299, 505);
+            this.btnNextImg.Name = "btnNextImg";
+            this.btnNextImg.Size = new System.Drawing.Size(75, 23);
+            this.btnNextImg.TabIndex = 7;
+            this.btnNextImg.Text = "Next >";
+            this.btnNextImg.UseVisualStyleBackColor = true;
+            this.btnNextImg.Click += new System.EventHandler(this.btnNextImg_Click);
+            // 
+            // btnScale
+            // 
+            this.btnScale.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnScale.Location = new System.Drawing.Point(218, 505);
+            this.btnScale.Name = "btnScale";
+            this.btnScale.Size = new System.Drawing.Size(75, 23);
+            this.btnScale.TabIndex = 6;
+            this.btnScale.Text = "Scale";
+            this.btnScale.UseVisualStyleBackColor = true;
+            this.btnScale.Click += new System.EventHandler(this.btnScale_Click);
+            // 
+            // btnPrevImg
+            // 
+            this.btnPrevImg.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnPrevImg.Location = new System.Drawing.Point(137, 505);
+            this.btnPrevImg.Name = "btnPrevImg";
+            this.btnPrevImg.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevImg.TabIndex = 5;
+            this.btnPrevImg.Text = "< Prev";
+            this.btnPrevImg.UseVisualStyleBackColor = true;
+            this.btnPrevImg.Click += new System.EventHandler(this.btnPrevImg_Click);
             // 
             // SearchTab
             // 
             this.SearchTab.Location = new System.Drawing.Point(4, 22);
             this.SearchTab.Name = "SearchTab";
             this.SearchTab.Padding = new System.Windows.Forms.Padding(3);
-            this.SearchTab.Size = new System.Drawing.Size(543, 520);
+            this.SearchTab.Size = new System.Drawing.Size(511, 536);
             this.SearchTab.TabIndex = 1;
             this.SearchTab.Text = "Search";
             this.SearchTab.UseVisualStyleBackColor = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.bGroupTagging);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.TabContainer);
+            this.splitContainer1.Size = new System.Drawing.Size(784, 562);
+            this.splitContainer1.SplitterDistance = 261;
+            this.splitContainer1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 562);
-            this.Controls.Add(this.TabContainer);
-            this.Controls.Add(this.lblDir);
-            this.Controls.Add(this.bGroupTagging);
+            this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.Text = "Form1";
             this.bGroupTagging.ResumeLayout(false);
             this.bGroupTagging.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
             this.TabContainer.ResumeLayout(false);
             this.ViewTab.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -192,12 +262,17 @@
         private System.Windows.Forms.RadioButton rbtnSFW;
         private System.Windows.Forms.Label lblSafe;
         private System.Windows.Forms.Label lblDir;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Label lblTags;
         private System.Windows.Forms.TabControl TabContainer;
         private System.Windows.Forms.TabPage ViewTab;
         private System.Windows.Forms.TabPage SearchTab;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnNextImg;
+        private System.Windows.Forms.Button btnScale;
+        private System.Windows.Forms.Button btnPrevImg;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- Abandon_All_Hope/Abandon_All_Hope/Form1.cs
+++ Abandon_All_Hope/Abandon_All_Hope/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,16 +12,35 @@ namespace Abandon_All_Hope
 {
     public partial class Form1 : Form
     {
+        /*
+        Load Image Directory / Current Image
+        protected string[] pFileNames;
+        protected int pCurrentImage = -1;
+        */
+
+        List<string> files;
+        int index = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        //######################Fix shit in here
         private void btnOpen_Click(object sender, EventArgs e)
         {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            string dirPath = fbd.ToString();
+            files = new List<string>(Directory.EnumerateFiles(dirPath));
+            picBox1.Image = Bitmap.FromFile(files[index]);
+            lblDir.Text = files[index];
+
+            
+            /* DEBUG 1######################
             OpenFileDialog ofd = new OpenFileDialog();
 
-            ofd.InitialDirectory = "C:\\";
+            ofd.Multiselect = true;
             ofd.Title = "Select Image";
             ofd.Filter = "All Files (*.*)|*.*|JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
 
@@ -30,7 +50,9 @@ namespace Abandon_All_Hope
                 // and assign that to the PictureBox.Image property
                 try
                 {
-                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                    pFileNames = ofd.FileNames;
+                    pCurrentImage = 0;
+                    ShowCurrentImage();
                 }
                 catch (System.ArgumentException error)
                 {
@@ -39,7 +61,38 @@ namespace Abandon_All_Hope
                 }
                 lblDir.Text = ofd.FileName;
             }
-            ofd.Dispose();
+            */
         }
+
+        // Previous Image in Directory
+        private void btnPrevImg_Click(object sender, EventArgs e)
+        {
+            --index;
+            if (index < 0)
+                index = files.Count - 1;
+
+            picBox1.Image = Bitmap.FromFile(files[index]);
+            lblDir.Text = files[index];
+        }
+
+        // Scale Image between CenterImage and Zoom(Fit)
+        private void btnScale_Click(object sender, EventArgs e)
+        {
+            if (picBox1.SizeMode == PictureBoxSizeMode.CenterImage)
+                picBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            else
+                picBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+
+        // Next Image in Directory
+        private void btnNextImg_Click(object sender, EventArgs e)
+        {
+            ++index;
+            if (index >= files.Count)
+                index = 0;
+            picBox1.Image = Bitmap.FromFile(files[index]);
+            lblDir.Text = files[index];
+        }
+
     }
 }
